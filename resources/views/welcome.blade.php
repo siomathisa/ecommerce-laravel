66e04f8946482e80f2ce1451bfee3747e96ed527
Mathis <vidueiramathis@gmail.com>
2025-10-18 04:49:32 +0200

    title

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bienvenue sur {{ config('app.name', 'E-Commerce Laravel') }}</title>
+    <title>{{ config('app.name', 'E-Commerce Laravel') }}</title>
     @vite('resources/css/app.css')
 </head>
 <body class="bg-gray-100 flex flex-col items-center justify-center min-h-screen text-center">
